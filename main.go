e6c6a47b87b0dccb490ca1f961b45cd05de55cf4
eharris128 <echarris@smcm.edu>
2024-08-28 18:40:39 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type status int
+
+const (
+	todo status = iota
+	inProgress
+	done
+)
+
+type Task struct {
+	status      status
+	title       string
+	description string
+}
+
+func (t Task) FilterValue() string {
+	return t.title
+}
+
+func (t Task) Title() string {
+	return t.title
+}
+
+func (t Task) Description() string {
+	return t.description
+}
+
+type Model struct {
+	list list.Model
+	err  error
+}
+
+func New() *Model {
+	// Initialize list with some default dimensions (can be adjusted as needed)
+	m := &Model{}
+	m.initList(80, 24) // Default dimensions
+	return m
+}
+
+func (m *Model) initList(width, height int) {
+	m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
+	m.list.Title = "To Do"
+	m.list.SetItems([]list.Item{
+		Task{status: todo, title: "Buy oats", description: "organic oats"},
+		Task{status: todo, title: "Buy mate", description: "tony mate"},
+		Task{status: todo, title: "do laundry", description: "30 degrees C and dry outdoors"},
+	})
+}
+
+func (m Model) Init() tea.Cmd {
+	// Optionally, you can return some initialization command here
+	return nil
+}
+
+func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.initList(msg.Width, msg.Height)
+	}
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m Model) View() string {
+	return m.list.View()
+}
+
+func main() {
+	m := New()
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
